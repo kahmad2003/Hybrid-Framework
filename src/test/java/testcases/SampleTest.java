22fcd76aa217250fbaffd67e932bb7eb2ba1e9a4
kahmad <kahmad2003@yahoo.com>
2026-01-09 17:58:14 -0600

    added a new file

---
+++ src/test/java/testcases/SampleTest.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sampleTestMethod() 
+	{
+		System.out.println("This is a sample test method");
+	}
+	
+	@Test
+	public void sampleTestMethod2() 
+	{
+		System.out.println("This is a sample test method 2");
+	}
+	
+	@Test
+	public void sampleTestMethod3() 
+	{
+		System.out.println("This is a sample test method 3");
+	}
+
+}
